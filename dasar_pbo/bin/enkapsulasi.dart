1e44705e6045f8fb2ac97732aa0cc9ca114f868b
agungtsmn <0812agung@gmail.com>
2021-03-21 19:15:03 +0700

    Enkapsulasi

---
+++ dasar_pbo/bin/enkapsulasi.dart
@@ -0,0 +1,51 @@
+// Amanda Agung Permata
+// 6304191217
+// Pemrograman Berorientasi Obyek
+
+// --------------------------------------------
+// Enkapsulasi
+
+// import 'dart:io';
+// import 'package:dasar_pbo/persegi_panjang.dart';
+
+// main(List<String> arguments) {
+//   PersegiPanjang kotak1, kotak2;
+//   double luasKotak1;
+
+//   kotak1 = new PersegiPanjang();
+//   kotak1.setPanjang(2);
+//   kotak1.lebar = -3;
+
+//   kotak2 = PersegiPanjang();
+//   kotak2.setPanjang(double.tryParse(stdin.readLineSync()));
+//   kotak2.lebar = double.tryParse(stdin.readLineSync());
+
+//   luasKotak1 = kotak1.hitungLuas();
+
+//   print(luasKotak1 + kotak2.hitungLuas());
+//   print(kotak1.lebar);
+// }
+
+// --------------------------------------------
+// Enkapsulasi 2
+
+// import 'dart:io';
+// import 'package:dasar_pbo/persegi_panjang.dart';
+
+// main(List<String> arguments) {
+//   PersegiPanjang kotak1, kotak2;
+//   double luasKotak1;
+
+//   kotak1 = new PersegiPanjang();
+//   kotak1.setPanjang(2);
+//   kotak1.lebar = -3;
+
+//   kotak2 = PersegiPanjang();
+//   kotak2.setPanjang(double.tryParse(stdin.readLineSync()));
+//   kotak2.lebar = double.tryParse(stdin.readLineSync());
+
+//   luasKotak1 = kotak1.luas;
+
+//   print(luasKotak1 + kotak2.luas);
+//   print(kotak1.lebar);
+// }
