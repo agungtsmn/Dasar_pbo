be3de38fd3fd7be3c87d8103831566f29d944e9f
agungtsmn <0812agung@gmail.com>
2021-03-21 19:15:14 +0700

    Persegi Panjang

---
+++ dasar_pbo/lib/persegi_panjang.dart
@@ -0,0 +1,73 @@
+// Amanda Agung Permata
+// 6304191217
+// Pemrograman Berorientasi Obyek
+
+// --------------------------------------------
+// Persegi Panjang
+
+// class PersegiPanjang {
+//   double _panjang;
+//   double _lebar;
+
+//   void set lebar(double value) {
+//     if (value < 0) {
+//       value *= -1;
+//     }
+//     _lebar = value;
+//   }
+
+//   void setPanjang(double value) {
+//     if (value < 0) {
+//       value *= -1;
+//     }
+//     _panjang = value;
+//   }
+
+//   double get lebar {
+//     return _lebar;
+//   }
+
+//   double getPanjang() {
+//     return _panjang;
+//   }
+
+//   double hitungLuas() {
+//     return this._panjang * _lebar;
+//   }
+// }
+
+// --------------------------------------------
+// Persegi Panjang 2
+
+// class PersegiPanjang {
+//   double _panjang;
+//   double _lebar;
+
+//   void set lebar(double value) {
+//     if (value < 0) {
+//       value *= -1;
+//     }
+//     _lebar = value;
+//   }
+
+//   void setPanjang(double value) {
+//     if (value < 0) {
+//       value *= -1;
+//     }
+//     _panjang = value;
+//   }
+
+//   double get lebar {
+//     return _lebar;
+//   }
+
+//   double getPanjang() {
+//     return _panjang;
+//   }
+
+//   double hitungLuas() {
+//     return this._panjang * _lebar;
+//   }
+
+//   double get luas => _panjang * _lebar;
+// }
