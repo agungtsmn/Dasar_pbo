1b67d18f70a4c6d5a2fbfd9a9fc5cfcc3613f130
agungtsmn <0812agung@gmail.com>
2021-03-21 20:34:46 +0700

    drink

---
+++ dasar_pbo/lib/drink_ability_mixin.dart
@@ -0,0 +1,10 @@
+// Amanda Agung Permata
+// 6304191217
+// Pemrograman Berorientasi Obyek
+
+import 'package:dasar_pbo/hero.dart';
+import 'hero.dart';
+
+mixin DrinkAbilityMixin on Hero {
+  String drink() => "Gluk.. Gluk.. Gluk..";
+}
